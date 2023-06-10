287b32024b597ee44efa3990a17f490d3e86b7f3
Misbah Ahmed <72851859+misbahofficial@users.noreply.github.com>
2023-06-10 23:08:42 +0600

    Comments added

--- extra_terrestrials.py
+++ extra_terrestrials.py
@@ -1,3 +1,8 @@
+'''
+This is a sololearn code coach problem which falls under easy category.
+The problem states that: You meet a group of aliens, and their language is just like English except that they say backwards.
+Now, the task is take your input and reverse it for them to understand.
+'''
 # function for reversing a string
 def reverse_str(a_string):
     # using string slicing to reverse a string
